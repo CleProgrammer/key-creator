bb4cb936aab8af3c1566bdf82aae58e494a3a11b
CleProgrammer <clebsonmengao@hotmail.com>
2023-09-25 10:38:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const c = (cl) => document.querySelector(cl)
+
+  const [saveKey, setsaveKey] = useState([])
+
+  let keyOptions = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  let keyOptionsSpecial = '@#$%&'
+  let keyOptionsNumber = '0123456789'
+  let key = ''
+
+  function calcular() {
+      key = ''
+
+      for(let i = 10; i > 0; i --) {
+          let keyGenerate = Math.floor(Math.random() * 52)
+          key += keyOptions[keyGenerate]
+      }
+
+      //Gerar Número
+      let keyGenerateNumber = Math.floor(Math.random() * 10)
+      let keyGenerateNumber2 = Math.floor(Math.random() * 10)
+      let key2 = key.replace(key[keyGenerateNumber], keyOptionsNumber[keyGenerateNumber2])
+
+      //Gerar caractere especial
+      let keyGenerate = Math.floor(Math.random() * 10)
+      let keyGenerate2 = Math.floor(Math.random() * 5)
+      let key3 = key2.replace(key2[keyGenerate], keyOptionsSpecial[keyGenerate2])
+
+      c('.key-result').innerHTML = 'Carregando...'
+      setTimeout(() => {
+        setsaveKey( key3 )
+      }, 2000)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='key-container'>
+        <div className='title-key'>Criador de Senha</div>
+        <div className='generate-key' onClick={calcular}>Criar Senha</div>
+        <div className='key-result'>{saveKey}</div>
+      </div>
     </div>
   );
 }
